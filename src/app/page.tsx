2000e181ae8d47d8f3c5d66c3956e9659fb08366
zyxkemren <daffa.adli212@gmail.com>
2026-03-01 17:25:37 +0700

    ya

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -31,11 +31,12 @@ export default function HomePage() {
   if (error) return <h1>Waduh, ada masalah pas ambil data.</h1>;
 
   return (
-    <main>
+    <main className="overflow-x-hidden">
+      <div className="background"></div>
       <Navbar active={1} logo="OSIS AL Bayan" />
-      <div className="start width-full h-[80vh] flex flex-col items-start justify-between bg-red-500 p-20">
-        <div className="start-menu w-full bg-yellow-100 h-[100px] flex flex-row justify-between items-center">
-          <div>OSIS AL Bayan</div>
+      <div className="start w-full h-[80vh] flex flex-col items-start justify-between px-30 pb-25 pt-25">
+        <div className="start-menu w-full h-[100px] flex flex-row justify-between items-center">
+          <h2>SMA PU Albayan Cibadak</h2>
           <div className="logo flex flex-row items-center gap-4">
             <span>
               <Image src="/img/osis.png" alt="Logo" width={50} height={50} />
@@ -44,6 +45,16 @@ export default function HomePage() {
               <Image src="/img/alba.png" alt="Logo" width={60} height={50} />
             </span>
           </div>
+        </div>
+        <div className="banner">
+          <Image
+            src="/img/abcd25.png" // Ganti dengan path gambarmu
+            alt="Albacadabra Background"
+            layout="fill"
+            objectFit="fit"
+            objectPosition="bottom"
+            priority
+          />
         </div>
         <div className="start-arrow flex flex-row justify-between w-full">
           <BiSolidLeftArrow className="text-white text-4xl" />
@@ -57,9 +68,9 @@ export default function HomePage() {
           <div className="bg-white w-full text-center px-full py-[10px] rounded-[50px] cursor-pointer">Learn more</div>
         </div>
       </div>
-      <div className="flex flex-row justify-between bg-yellow-300 w-full p-30 gap-[5vw]">
+      <div className="flex flex-row justify-between bg-[#001f3f] w-full p-50 gap-[5vw]">
         <div className="flex flex-col max-w-[60vw] gap-[25px]">
-          <span className="text-[2rem] font-bold">Introduction to Arthawisesa</span>
+          <h2 className="text-[3rem] font-bold">Welcome to Arthawisesa</h2>
           <p className="whitespace-pre-line">
             {`Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever
             since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.\n\nIt has survived not only
@@ -80,10 +91,15 @@ export default function HomePage() {
           <p className="mt-4 text-center font-medium text-gray-700">🎥 After Movie Nasionalismeku 2024</p>
         </div>
       </div>
-      <div className="w-full px-30 py-15 bg-red-500 flex flex-row">
-        <div className="w-[400px] h-[250px] bg-white rounded-[20px]"></div>
-        <div></div>
+      <div className="w-full px-50 py-10 flex flex-col  bg-[#18191b] ">
+        <span className="text-center text-white text-[1.5rem] font-black">Follow our social media</span>
+        <div className="w-full pt-10 flex flex-row justify-between gap-[3vw]">
+          <div className="w-full h-[100px] border-[3px] border-white rounded-[20px]"></div>
+          <div className="w-full h-[100px] bg-white rounded-[20px]"></div>
+          <div className="w-full h-[100px] border-[3px] border-white rounded-[20px]"></div>
+        </div>
       </div>
+      <div></div>
     </main>
   );
 }
