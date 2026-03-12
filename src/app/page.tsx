fade87e763f4eb2650c1cc1023028221f44af9e8
zyxkemren <daffa.adli212@gmail.com>
2026-03-12 11:48:35 +0700

    gtw dah

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,7 @@
 import { getData } from "@/lib/firebase/firebase";
 import { useState, useEffect } from "react";
 import { BiSolidRightArrow, BiSolidLeftArrow } from "react-icons/bi";
+import { FaInstagram, FaYoutube } from "react-icons/fa6";
 import Navbar from "@/components/ui/navbar";
 import Image from "next/image";
 import "./main.css";
@@ -28,13 +29,20 @@ export default function HomePage() {
     fetchData();
   }, []);
 
-  if (loading) return <h1>Loading...</h1>;
-  if (error) return <h1>Waduh, ada masalah pas ambil data.</h1>;
+  if (loading)
+    return (
+      <div className="bouncing-loader">
+        <div></div>
+        <div></div>
+        <div></div>
+      </div>
+    );
+  if (error) return <h1>error bos</h1>;
 
   return (
     <main className="overflow-x-hidden">
       <Navbar active={1} logo="OSIS AL Bayan" />
-      <div className="start w-full h-[100vh] flex flex-col items-start justify-between px-30 pb-25 pt-25">
+      <div className="start w-full h-[100vh] flex flex-col items-start justify-between py-25">
         <div className="banner">
           <Image
             src="/img/_MG_7205.jpg.jpeg" // Ganti dengan path gambarmu
@@ -46,11 +54,15 @@ export default function HomePage() {
             priority
           />
         </div>
-        <div className="start-arrow flex flex-row justify-between w-full">
-          <BiSolidLeftArrow className="text-white text-4xl" />
-          <BiSolidRightArrow className="text-white text-4xl" />
+        <div className="start-arrow flex flex-row justify-between w-full px-[50px]">
+          <div className="btn-next left">
+            <BiSolidLeftArrow />
+          </div>
+          <div className="btn-next right">
+            <BiSolidRightArrow />
+          </div>
         </div>
-        <div className="flex flex-row items-center justify-between w-full z-2">
+        <div className="flex flex-row items-center justify-between w-full z-2 px-30">
           <div className="flex flex-col items-center justify-end gap-3">
             <div className="flex flex-col">
               <span className="text-gray-200">Our event</span>
@@ -71,7 +83,7 @@ export default function HomePage() {
       <div className="relative z-1 w-full h-[200px] bg-gradient-to-t from-[#001f3f] to-transparent -mt-[280px]"></div>
       <div className="flex flex-row justify-between bg-[#001f3f] w-full p-50 gap-[5vw]">
         <div className="flex flex-col max-w-[60vw] gap-[25px]">
-          <h2 className="text-[3rem] font-bold">Welcome to Arthawisesa</h2>
+          <h1>Welcome to Arthawisesa</h1>
           <p className="whitespace-pre-line">
             {`Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever
             since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.\n\nIt has survived not only
@@ -95,15 +107,23 @@ export default function HomePage() {
         </div>
       </div>
       <div className="w-full px-50 py-10 flex flex-col  bg-[#18191b]">
-        <span className="text-center text-white text-[1.5rem] font-black">Follow our social media</span>
-        <div className="w-full pt-10 flex flex-row justify-between gap-[3vw]">
-          <div className="w-full h-[100px] border-[3px] border-white rounded-[20px]"></div>
-          <div className="w-full h-[100px] bg-white rounded-[20px]"></div>
-          <div className="w-full h-[100px] border-[3px] border-white rounded-[20px]"></div>
+        <div className="w-full flex flex-row justify-center gap-[3vw]">
+          <div className="sosmed">
+            <FaInstagram />
+            <span>Instagram OSIS</span>
+          </div>
+          <div className="sosmed utama">
+            <FaYoutube />
+            <span>Youtube OSIS</span>
+          </div>
+          <div className="sosmed">
+            <FaInstagram />
+            <span>Instagram MPK</span>
+          </div>
         </div>
       </div>
       <div className="cabinet py-30 px-50">
-        <h2 className="text-[2rem] font-bold !text-[#304356] mb-10">Meet our cabinet</h2>
+        <h2 className="text-[2rem] font-bold !text-[#304356] mb-10">Meet Our Cabinet</h2>
         <div className="w-full flex flex-row gap-4 overflow-x-auto pb-4 custom-scroll">
           <div className="boxes">
             <div className="ministry">Kementrian Kontol</div>
