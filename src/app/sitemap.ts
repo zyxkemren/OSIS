8088e0beefd7c714ac397be2768c1fc6ebd37192
zyxkemren <daffa.adli212@gmail.com>
2026-04-12 08:08:08 +0700

    sitemap test

---
+++ src/app/sitemap.ts
@@ -0,0 +1,14 @@
+import { MetadataRoute } from 'next'
+
+export default function sitemap(): MetadataRoute.Sitemap {
+  const baseUrl = 'https://osisalbayan.com' // Ganti dengan domainmu
+
+  return [
+    {
+      url: baseUrl,
+      lastModified: new Date(),
+      changeFrequency: 'yearly',
+      priority: 1,
+    },
+  ]
+}
